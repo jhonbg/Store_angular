eac937f45aa690ea401220680475af5680cb1617
jhonbg <jhonbg7@gmail.com>
2025-06-19 15:44:18 -0500

    recurcibidad en paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,26 @@
 import { Routes } from '@angular/router';
 
 import { ListComponent } from '@products/pages/list/list.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component'
 import { AboutComponent } from '@info/pages/about/about.component';
 import { NotFoundComponent } from '@info/pages/not-found/not-found.component'
 
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                component: ListComponent
+            },
+            {
+                path: 'about',
+                component: AboutComponent
+            },
+        ]
+    },
     {
         path: '',
         component: ListComponent
